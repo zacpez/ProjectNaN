51c7bff8eb66daf162ec940080ff88b48abf2277
zacpez <zacpez@gmail.com>
2020-06-30 09:25:51 -0700

    Add port argument

--- server/start.js
+++ server/start.js
@@ -1,13 +1,17 @@
 var HttpService = require('./server'),
-    WebSocket = require('./socket'),
-    GameServer = require('./gameserver');
+	WebSocket = require('./socket'),
+	GameServer = require('./gameserver');
 
-
+var argv = process.argv.splice(2);
 var server = new HttpService();
-server.initialize( '0.0.0.0', 9081 );
+var port = 9081;
+if (argv.length) {
+	port = argv[0];
+}
+server.initialize('0.0.0.0', port);
 
 var gameServer = new GameServer();
 gameServer.initialize();
 
 var socket = new WebSocket();
-socket.initialize( server.http, gameServer );
+socket.initialize(server.http, gameServer);
